a68d9dcbcf44c05a443c834caf6cc055b1967055
sai umesh <saiumesh.dhanewar@apty.io>
2022-01-21 14:44:07 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# GitHub action to get bundle keys
+
+## usage
+
+```cmd
+      - name: Get split key
+        uses: aptyInc/action-multi-eks@master
+        id: keys
+        env:
+          REGION: <region>
+          BRANCH_NAME: <branch-name>
+```
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/actions-go/toolkit/core"
+)
+
+func runMain() {
+	region := os.Getenv("REGION")
+	branchName := os.Getenv("BRANCH_NAME")
+
+	macBuildBranches := []string{"staging", "production", "labs"}
+
+	IS_CROSS_REGION := "false"
+	IS_MAC_BUILD_BRANCH := "false"
+
+	if region == "" || branchName == "" {
+		log.Fatal("REGION and BRANCH_NAME must be set")
+	}
+
+	if branchName == "production" && region != "us-east-1" {
+		IS_CROSS_REGION = "true"
+		IS_MAC_BUILD_BRANCH = "true"
+	}
+
+	if region == "us-east-1" {
+		for _, branch := range macBuildBranches {
+			if branch == branchName {
+				IS_MAC_BUILD_BRANCH = "true"
+				break
+			}
+		}
+	}
+
+	core.SetOutput("IS_CROSS_REGION", IS_CROSS_REGION)
+	core.SetOutput("IS_MAC_BUILD_BRANCH", IS_MAC_BUILD_BRANCH)
+}
+
+func main() {
+	runMain()
+}
